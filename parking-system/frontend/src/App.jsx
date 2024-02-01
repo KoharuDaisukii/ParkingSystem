3cfe9307a6fdf361ce9cf3bd2f730c3bc9576f3e
Soroso <tudgus010707@naver.com>
2024-02-01 19:48:53 +0900

    라우트 설정

--- parking-system/frontend/src/App.jsx
+++ parking-system/frontend/src/App.jsx
@@ -3,20 +3,29 @@ import Input from "./components/Input";
 import ParkList from "./components/ParkList";
 import MainRoute from "./routes/MainRoute";
 import Login from "./components/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./pages/Root";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <MainRoute />,
+      },
+      { path: "login", element: <Login /> },
+    ],
+  },
+]);
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainRoute />}></Route>
-          <Route path="/login/" element={<Login />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
