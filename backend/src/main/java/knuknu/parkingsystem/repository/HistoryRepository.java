900949a5d4a640de1f2a3d060d7aa9fffd3fa02d
KoharuDaisukii <bong1926@naver.com>
2024-02-01 20:08:24 +0900

    큰일날뻔

--- backend/src/main/java/knuknu/parkingsystem/repository/HistoryRepository.java
+++ backend/src/main/java/knuknu/parkingsystem/repository/HistoryRepository.java
@@ -3,13 +3,11 @@ package knuknu.parkingsystem.repository;
 import java.util.List;
 import java.util.Optional;
 
-import knuknu.parkingsystem.controller.HistoryForm;
 import knuknu.parkingsystem.domain.History;
 
 public interface HistoryRepository {
-	int save(HistoryForm historyForm);
-	Optional<History> findHistoryById(int id);
-	byte[] findPhotoById(int id);
+	int save(History history);
+	Optional<History> findById(int id);
 	List<History> findByAdminID(String admin_id);
 	List<History> findByCarID(String car_no);
 	List<History> findAll();
--- backend/src/main/java/knuknu/parkingsystem/service/HistoryService.java
+++ backend/src/main/java/knuknu/parkingsystem/service/HistoryService.java
@@ -1,41 +1,38 @@
 package knuknu.parkingsystem.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import knuknu.parkingsystem.controller.HistoryForm;
 import knuknu.parkingsystem.domain.History;
 import knuknu.parkingsystem.repository.HistoryRepository;
 
 @Service
 public class HistoryService {
 	private final HistoryRepository historyRepository;
-
+	
 	@Autowired
-	public HistoryService(HistoryRepository historyRepository) {
+	public HistoryService(HistoryRepository historyRepository)
+	{
 		this.historyRepository = historyRepository;
 	}
-
+	
 	// 주차
-	public int park(HistoryForm historyForm) {
-		return historyRepository.save(historyForm);
-	}
-
-	// 전체 주차 내역 조회
-	public List<History> findHistories() {
-		return historyRepository.findAll();
-	}
-
-	// 미납 차랑 조회
-	public List<History> findUnpaid() {
-		return historyRepository.findNotPaid();
+	public int park(History history)
+	{
+		historyRepository.save(history);
+		return history.getId();
 	}
 	
-	// 사진 조회
-	public byte[] findImage(int id){
-		return historyRepository.findPhotoById(id);
+	// 전체 주차 내역 조회
+	public List<History> findHistories()
+	{
+		return historyRepository.findAll();
 	}
+	
+	public List<History> findUnpaid()
+	{
+		return historyRepository.findNotPaid()
+;	}
 }
