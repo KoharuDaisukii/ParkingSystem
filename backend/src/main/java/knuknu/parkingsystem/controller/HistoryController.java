c128e11ae9625a6f20b99759f1ba22ac71676fe1
KoharuDaisukii <bong1926@naver.com>
2024-02-02 22:44:15 +0900

    park in 추가

--- backend/src/main/java/knuknu/parkingsystem/controller/HistoryController.java
+++ backend/src/main/java/knuknu/parkingsystem/controller/HistoryController.java
@@ -1,9 +1,15 @@
 package knuknu.parkingsystem.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import knuknu.parkingsystem.domain.History;
@@ -12,24 +18,32 @@ import knuknu.parkingsystem.service.HistoryService;
 @RestController
 public class HistoryController {
 	private final HistoryService historyService;
-	
+
 	@Autowired
-	public HistoryController(HistoryService historyService)
-	{
+	public HistoryController(HistoryService historyService) {
 		this.historyService = historyService;
 	}
-	
+
 	@GetMapping("/history")
-	public List<History> readAll()
-	{
+	public List<History> readAll() {
 		List<History> histories = historyService.findHistories();
 		return histories;
 	}
-	
+
 	@GetMapping("/history/unpaid")
-	public List<History> readUnpaid()
-	{
+	public List<History> readUnpaid() {
 		List<History> histories = historyService.findUnpaid();
 		return histories;
+	}
+
+	@GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
+	public byte[] getImage(@RequestParam(value = "id", defaultValue = "1") int id) {
+		return historyService.findImage(id);
+	}
+	
+	@PostMapping(value = "park/in", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+	public int createHistory(@ModelAttribute HistoryForm historyForm)
+	{
+		return historyService.park(historyForm);
 	}
 }
--- backend/src/main/java/knuknu/parkingsystem/service/SpringConfig.java
+++ backend/src/main/java/knuknu/parkingsystem/service/SpringConfig.java
@@ -3,9 +3,14 @@ package knuknu.parkingsystem.service;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
+import jakarta.servlet.MultipartConfigElement;
 import knuknu.parkingsystem.repository.HistoryRepository;
 import knuknu.parkingsystem.repository.JdbcTemplateHistoryRepository;
 
