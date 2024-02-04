9aefdec2d45464189972ac005da0e844573e8a52
KoharuDaisukii <bong1926@naver.com>
2024-02-04 22:45:40 +0900

    출차, id로 history 조회 추가

--- backend/src/main/java/knuknu/parkingsystem/controller/HistoryController.java
+++ backend/src/main/java/knuknu/parkingsystem/controller/HistoryController.java
@@ -1,13 +1,17 @@
 package knuknu.parkingsystem.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,10 +28,16 @@ public class HistoryController {
 		this.historyService = historyService;
 	}
 
-	@GetMapping("/history")
+	@GetMapping("/history/all")
 	public List<History> readAll() {
 		List<History> histories = historyService.findHistories();
 		return histories;
+	}
+	
+	@GetMapping("/history")
+	public Optional<History> readHistoryById(@RequestParam("id") int id)
+	{
+		return historyService.findHistoryById(id);
 	}
 
 	@GetMapping("/history/unpaid")
@@ -41,9 +51,15 @@ public class HistoryController {
 		return historyService.findImage(id);
 	}
 	
-	@PostMapping(value = "park/in", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+	@PostMapping(value = "/park/in", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
 	public int createHistory(@ModelAttribute HistoryForm historyForm)
 	{
-		return historyService.park(historyForm);
+		return historyService.parkIn(historyForm);
+	}
+	
+	@PutMapping("/park/out")
+	public Optional<History> updateExitTime(ExitTimeUpdateForm exitTimeUpdateForm)
+	{
+		return historyService.parkOut(exitTimeUpdateForm.getId(), exitTimeUpdateForm.getExit_time());
 	}
 }
